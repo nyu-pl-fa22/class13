3b5dc84df299743f30e7af69d4c2cae5c58c6443
Thomas Wies <wies@cs.nyu.edu>
2022-12-13 11:08:54 -0500

    first commit

---
+++ cmd/merge.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+func split(pin, pout *chan int) {
+	vall := <-*pin
+	valr, more := <-*pin
+	if more {
+		// Spawn off recursive sorters
+		lin := make(chan int)
+		lout := make(chan int)
+		go split(&lout, &lin)
+		rin := make(chan int)
+		rout := make(chan int)
+		go split(&rout, &rin)
+
+		// Send data from parent to recursive sorters
+		lout <- vall
+		rout <- valr
+		for val := range *pin {
+			lout <- val
+			lout, rout = rout, lout
+		}
+		close(lout)
+		close(rout)
+
+		merge(pout, &lin, &rin)
+
+	} else {
+		// Only one element to sort - just return it to parent
+		*pout <- vall
+		close(*pout)
+	}
+}
+
+func merge(pout, lin, rin *chan int) {
+	// Receive and merge values from recursive sorters
+	lval, lmore := <-*lin
+	rval, rmore := <-*rin
+	for rmore {
+		if rval <= lval {
+			*pout <- rval
+		} else {
+			*pout <- lval
+			lval = rval
+			lmore, rmore = rmore, lmore
+			lin, rin = rin, lin
+		}
+		rval, rmore = <-*rin
+	}
+	// Done with rin
+
+	// Copy remaining values from lin to pout
+	for lmore {
+		*pout <- lval
+		lval, lmore = <-*lin
+	}
+	close(*pout)
+}
+
+func main() {
+	input := [6]int{1, 4, 4, 2, 7, 3}
+
+	in := make(chan int)
+	out := make(chan int)
+
+	go split(&out, &in)
+
+	for _, val := range input {
+		out <- val
+	}
+	close(out)
+
+	for val := range in {
+		fmt.Println(val)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module merge
+
+go 1.19
+
+require github.com/markphelps/optional v0.10.0 // indirect
